c0e42fc328a908d47c3f0c5be8cd7816a99ddd42
SantiagoG117 <garc0254@algonquinlive.com>
2025-08-31 15:32:15 -0400

    Set up TailwindCSS

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -12,7 +12,7 @@ function App() {
 
   return (
     <div className="App">
-      <h1>{message}</h1>
+      <h1 className="font-bold">{message}</h1>
     </div>
   );
 }
--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   server: {
     // A proxy is used to redirect API calls to the backend server
     proxy: {
